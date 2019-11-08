819a822d1317327e84b9eea8656f6eee27152a75
Jlim331 <54864392+Jlim331@users.noreply.github.com>
2019-11-07 19:06:21 -0600

    Update to menu
    Updated to match PEP8 format

--- menu.py
+++ menu.py
@@ -8,7 +8,7 @@ def choosenPath(menuName):
     """Function that will take a dictionary, menuName, and converts it to a
     text-based menu
     """
-    for key in menuName: # Prints keys
+    for key in menuName:  # Prints keys
         if key == "description":
             print(f"{menuName[key].capitalize()}")
         else:
@@ -16,50 +16,51 @@ def choosenPath(menuName):
     # Input for choosing the menu
     option = input("Where would you like to search? ")
     option = option.lower()
-    options = list(menuName) # Creates a list of keys for error checking
-    options.remove("description") # removes description from options
+    options = list(menuName)  # Creates a list of keys for error checking
+    options.remove("description")  # removes description from options
     # Loop that checks if the user enter an invalid input or quit
     while option not in options or option == "quit":
-        if option =="quit": # if quit, quit
+        if option == "quit":  # if quit, quit
             print("I'll see you later ;)")
             break
-        else: # else print options and ask for input
+        else:  # else print options and ask for input
             print(f"I got, {str(options)[1:-1]} watchu want?")
             option = input("")
     for i in options:
-        if option == i: # Checks which path player chooses
-            print(f"You search the {option} and see\n") # prints message for player
-            menuLDesc(menuName, option) # prints out next menu
+        if option == i:  # Checks which path player chooses
+            print(f"You search the {option} and see\n")
+            menuLDesc(menuName, option)  # prints out next menu
+
 
 def menuLDesc(menuName, key):
     """Function that takes a dictionary, menuName, and converts it to a
     text-based menu for the key, key, only works if the key value paired of the
     key is a list
     """
-    print(f"{key.title()}:") # prints the option player choose
-    for items in menuName[key]: # prints the options that goes with the option
+    print(f"{key.title()}:")  # prints the option player choose
+    for items in menuName[key]:  # prints the options that goes with the option
         print(f"\t- {items}")
-    option = input("What would you like to search? ") # takes another input
+    option = input("What would you like to search? ")  # takes another input
     option = option.lower()
     # Checks if user entered a valid input or quit
     while option not in menuName[key] or option == "quit":
-        if option =="quit": # if quit, quit
+        if option == "quit":  # if quit, quit
             print("Have a good night hun")
             break
-        else: # else print options and ask for input
+        else:  # else print options and ask for input
             print(f"I got, {str(menuName[key])[1:-1]} watchu want?")
             option = input("")
-    for i in menuName[key]: # checks which path the player chose
-        if option == "go back": # if go back, reruns previous menu
+    for i in menuName[key]:  # checks which path the player chose
+        if option == "go back":  # if go back, reruns previous menu
             choosenPath(menuName)
 
 escapeRoom = {
-    "description":"you find yourself in a room, in it you see...",
-    "cabinet":["go back"],
-    "chest":["go back"],
-    "bookcase":["go back"],
-    "painting":["go back"],
-    "desk":["go back"],
+    "description": "you find yourself in a room, in it you see...",
+    "cabinet": ["go back"],
+    "chest": ["go back"],
+    "bookcase": ["go back"],
+    "painting": ["go back"],
+    "desk": ["go back"],
 }
 
 choosenPath(escapeRoom)
